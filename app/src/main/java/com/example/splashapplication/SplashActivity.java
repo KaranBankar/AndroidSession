411422e1534896d22730cc07d33400594772beed
KaranBankar <karanbankar54@gmail.com>
2024-10-16 22:05:37 +0530

    comment Added

--- app/src/main/java/com/example/splashapplication/SplashActivity.java
+++ app/src/main/java/com/example/splashapplication/SplashActivity.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//This Comment is for GitHub
 public class SplashActivity extends AppCompatActivity {
 
     @Override  //Annotation
