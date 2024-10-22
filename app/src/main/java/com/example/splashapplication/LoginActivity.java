166f8c0e00227171ebd2a3ce2f229188f4c0f10f
KaranBankar <karanbankar54@gmail.com>
2024-10-22 22:04:01 +0530

    Login Page Backend Developed

--- app/src/main/java/com/example/splashapplication/LoginActivity.java
+++ app/src/main/java/com/example/splashapplication/LoginActivity.java
@@ -3,6 +3,11 @@ package com.example.splashapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 //New Commenty
 public class LoginActivity extends AppCompatActivity {
+
+    EditText e1_username,et2_password;
+    Button login_button;
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,45 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        e1_username=findViewById(R.id.username);
+        et2_password=findViewById(R.id.password);
+        login_button=findViewById(R.id.btn_login);
+        txt=findViewById(R.id.sign);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(e1_username.getText().toString().isEmpty()){
+                    e1_username.setError("username cannnot be Empty");
+                }
+                else if(e1_username.getText().toString().length()<5){
+                    e1_username.setError("User must be grater than 5 letters");
+                }
+                else if(et2_password.getText().toString().isEmpty()){
+                    et2_password.setError("Password cannot empty");
+                }
+                else if(et2_password.getText().toString().length()<5){
+                    et2_password.setError("Password must be grater̥ than 5 Letters");
+                }
+                else{
+                    Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+
+
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+                finish();
+            }
         });
 
     }
